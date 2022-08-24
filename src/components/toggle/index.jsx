d3f5547ba588a6818c475ec8d9ae5a5c030e0412
Djimo van Berlo <djimovanberlo@gmail.com>
2022-08-24 13:07:25 +0200

    fix toggle

--- src/components/toggle/index.jsx
+++ src/components/toggle/index.jsx
@@ -4,7 +4,7 @@ const Toggle = () => {
   const { theme, toggleTheme } = useThemeContext()
 
   return (
-    <div className='toggle' onClick={toggleTheme}>
+    <div className={`toggle toggle--${theme}`} onClick={toggleTheme}>
       <div className={`toggle__circle toggle__circle--${theme}`} />
     </div>
   )
