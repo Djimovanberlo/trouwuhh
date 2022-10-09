3cc9f988d5c5f7208e8bbd5621d09fc0382fe866
Djimo van Berlo <djimovanberlo@gmail.com>
2022-10-09 12:19:38 +0200

    fix btn link

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -8,7 +8,7 @@ export const Button = props => {
 
 export const ButtonLink = props => {
   const { theme } = useThemeContext()
-  return theme === 'belgium' ? <ButtonBelgium {...props} /> : <ButtonCheesy {...props} />
+  return theme === 'belgium' ? <ButtonLinkBelgium {...props} /> : <ButtonLinkCheesy {...props} />
 }
 
 export const ButtonCheesy = ({ type = null, className = '', onClick = null, children }) => (
@@ -23,11 +23,14 @@ export const ButtonBelgium = ({ type = null, className = '', onClick = null, chi
   </button>
 )
 
-export const ButtonLinkCheesy = ({ className = '', to = null, children }) => (
-  <Link className={`button button--cheesy ${className}`} to={to}>
-    {children}
-  </Link>
-)
+export const ButtonLinkCheesy = ({ className = '', to = null, children }) => {
+  console.log('TO', to)
+  return (
+    <Link className={`button button--cheesy ${className}`} to={to}>
+      {children}
+    </Link>
+  )
+}
 
 export const ButtonLinkBelgium = ({ className = '', to = null, children }) => (
   <Link className={`button button--belgium ${className}`} to={to}>
