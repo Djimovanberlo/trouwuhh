73ac051f9d1550fc9310446f9b6d423126f886ad
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-20 12:15:23 +0100

    fix click outside

--- src/layout/nav/nav-small.jsx
+++ src/layout/nav/nav-small.jsx
@@ -7,8 +7,9 @@ import { NAV_ITEMS } from '../../lib/constants'
 import { useThemeContext } from '../theme'
 import Hamburger from './hamburger'
 
-const NavList = ({ isNavOpen, toggleNavOpen }) => {
+const NavList = ({ isNavOpen, toggleNavOpen, closeNav }) => {
   const id = useId()
+  const ref = useRef(null)
   const navigate = useNavigate()
   const { theme, language } = useThemeContext()
 
@@ -17,22 +18,6 @@ const NavList = ({ isNavOpen, toggleNavOpen }) => {
     navigate(href)
     toggleNavOpen()
   }
-
-  return (
-    <div className={`nav-list nav-list--${theme} ${isNavOpen ? 'nav-list--open' : 'nav-list--closed'}`}>
-      {NAV_ITEMS.map(({ text, href }, index) => (
-        <div key={`${id}--${index}`} onClick={handleClick(href)}>
-          <P>{text[language]}</P>
-        </div>
-      ))}
-    </div>
-  )
-}
-
-const NavigationSmall = () => {
-  const ref = useRef(null)
-  const [isNavOpen, setNavIsOpen] = useState(false)
-  const toggleNavOpen = () => setNavIsOpen(!isNavOpen)
 
   const useOnClickOutside = (ref, handler) => {
     useEffect(() => {
@@ -51,14 +36,30 @@ const NavigationSmall = () => {
     }, [ref, handler])
   }
 
-  useOnClickOutside(ref, () => setNavIsOpen(false))
+  useOnClickOutside(ref, closeNav)
+
+  return (
+    <div ref={ref} className={`nav-list nav-list--${theme} ${isNavOpen ? 'nav-list--open' : 'nav-list--closed'}`}>
+      {NAV_ITEMS.map(({ text, href }, index) => (
+        <div key={`${id}--${index}`} onClick={handleClick(href)}>
+          <P>{text[language]}</P>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const NavigationSmall = () => {
+  const [isNavOpen, setNavIsOpen] = useState(false)
+  const toggleNavOpen = () => setNavIsOpen(!isNavOpen)
+  const closeNav = () => setNavIsOpen(false)
 
   return (
     <>
-      <div ref={ref} className='nav--small__wrapper'>
+      <div className='nav--small__wrapper'>
         <Hamburger isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} />
       </div>
-      <NavList isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} />
+      <NavList isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} closeNav={closeNav} />
     </>
   )
 }
