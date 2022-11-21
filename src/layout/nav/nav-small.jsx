5185693eb7eb206d1bebcb6a88b555ba6f47cf27
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-21 17:20:49 +0100

    fixed nav, removed click outisde

--- src/layout/nav/nav-small.jsx
+++ src/layout/nav/nav-small.jsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react'
-import { useEffect, useId, useState } from 'react'
+import { useId, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { P } from '../../components/typography'
 
@@ -7,7 +7,7 @@ import { NAV_ITEMS } from '../../lib/constants'
 import { useThemeContext } from '../theme'
 import Hamburger from './hamburger'
 
-const NavList = ({ isNavOpen, toggleNavOpen, closeNav }) => {
+const NavList = ({ isNavOpen, toggleNavOpen }) => {
   const id = useId()
   const ref = useRef(null)
   const navigate = useNavigate()
@@ -18,25 +18,6 @@ const NavList = ({ isNavOpen, toggleNavOpen, closeNav }) => {
     navigate(href)
     toggleNavOpen()
   }
-
-  const useOnClickOutside = (ref, handler) => {
-    useEffect(() => {
-      const listener = event => {
-        if (!ref.current || ref.current.contains(event.target)) {
-          return
-        }
-        handler(event)
-      }
-      document.addEventListener('mousedown', listener)
-      document.addEventListener('touchstart', listener)
-      return () => {
-        document.removeEventListener('mousedown', listener)
-        document.removeEventListener('touchstart', listener)
-      }
-    }, [ref, handler])
-  }
-
-  useOnClickOutside(ref, closeNav)
 
   return (
     <div ref={ref} className={`nav-list nav-list--${theme} ${isNavOpen ? 'nav-list--open' : 'nav-list--closed'}`}>
@@ -52,14 +33,13 @@ const NavList = ({ isNavOpen, toggleNavOpen, closeNav }) => {
 const NavigationSmall = () => {
   const [isNavOpen, setNavIsOpen] = useState(false)
   const toggleNavOpen = () => setNavIsOpen(!isNavOpen)
-  const closeNav = () => setNavIsOpen(false)
 
   return (
     <>
       <div className='nav--small__wrapper'>
         <Hamburger isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} />
       </div>
-      <NavList isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} closeNav={closeNav} />
+      <NavList isNavOpen={isNavOpen} toggleNavOpen={toggleNavOpen} />
     </>
   )
 }
